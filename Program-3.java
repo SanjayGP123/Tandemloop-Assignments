e129c3b32216566a284a004610896d5cbf402065
SanjayGP123 <sanjaygp281284@gmail.com>
2025-06-21 21:21:22 +0530

    Program-3.java

---
+++ Program-3.java
@@ -0,0 +1,26 @@
+package TandomloopAssignment;
+
+import java.util.Scanner;
+
+public class ThirdQuestion {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a value a : ");
+        int a = scanner.nextInt();
+        
+        int count = (a % 2 == 0) ? a - 1 : a;
+
+        for (int i = 0; i < count; i++) {
+            int odd = 2 * i + 1;
+            System.out.print(odd);
+            if (i < count - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        scanner.close();
+	}
+
+}
